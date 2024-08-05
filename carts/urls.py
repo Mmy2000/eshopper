6487c390b2e9b50726903378960b883195124825
Mmy2000 <yahianada005@gmail.com>
2024-08-05 22:52:10 +0300

    uploading the project

--- carts/urls.py
+++ carts/urls.py
@@ -12,9 +12,8 @@ urlpatterns = [
     #api
     path('api/cart/', api_view.CartAPIView.as_view(), name='cart_api'),
     path('api/add_to_cart/<int:product_id>/', api_view.AddToCart.as_view(), name='add_to_cart'),
-    path('api/decrement_cart/<int:product_id>/<int:cart_item_id>/', api_view.DecrementCart.as_view(), name='decrement_cart'),
-    path('api/delete_cart/<int:product_id>/<int:cart_item_id>/', api_view.DeleteCartItem.as_view(), name='delete_cart'),
-    path('api/checkout/', api_view.CheckoutAPIView.as_view(), name='cart_api'),
-
+    path('api/decrement_cart/<int:product_id>/<int:cart_item_id>/', api_view.DecrementCart.as_view(), name='decrement_cart_api'),
+    path('api/delete_cart/<int:product_id>/<int:cart_item_id>/', api_view.DeleteCartItem.as_view(), name='delete_cart_api'),
+    path('api/checkout/', api_view.CheckoutAPIView.as_view(), name='checkout_api'),
 
 ]
